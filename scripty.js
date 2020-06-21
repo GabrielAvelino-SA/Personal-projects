ced45ea517274d49f8adaa1dc3a8c2499ac3c0f5
Gabriel Avelino <62305317+GabrielAvelino-SA@users.noreply.github.com>
2020-06-20 23:20:56 -0300

    Upgrade
    upgrad layout and introduction of js

---
+++ scripty.js
@@ -0,0 +1,37 @@
+
+var resposta = resposta, numero, x, z;
+
+
+//1  usar teclas para digitar no hx
+//2  pegar valor de h*
+var calculo = document.getElementById("h3").innerText;
+window.alert(calculo);
+calculo.innerText("a");
+//3  realizar o calculo
+function set (a){
+    var numero;
+    numero = numero + a;
+    window.alert(numero);
+    calculo.innerText(numero)
+}
+
+function soma (y){
+    y = Number(y);
+    return x+=y;
+}
+function subtracao(y){
+    y = Number(y);
+    return x-=y
+}
+function multiplicacao(y){
+    y = Number(y);
+    return x*=y
+}
+function divisao (y){
+    return x/=y
+}
+function inserir (){
+   
+}
+//4  mostrar na tela o resultado
+//setar o valor do input pelo oque esta sendo digitado no key virtual
